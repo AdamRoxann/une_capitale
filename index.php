e77a2815978583cabf8b63d9da360f71730f0f05
AdamRoxann <adam354oke@gmail.com>
2020-04-30 19:55:26 +0700

    Footer

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 	<?php
 	 include "header.php"
 	 ?>
+	 <div class="wrap">
 	 <h1 class="font">Bon Appetit</h1>
 	 <p class="we">WE DON'T JUST COOK, WE TREAT YOU TO DELICACIES</p>
 	 <!-- <div class="vertical-center">
@@ -22,6 +23,12 @@
 	 <div class="button_cont" align="center"><a class="example_e" href="add-website-here" target="_blank" rel="nofollow noopener">BOOK NOW WITH US</a></div>
 	 <!-- <button>GET</button> -->
 	 <!-- <div id="json"></div> -->
+	 </div>
+	 <footer class="footer">
+      <div class="text-center">Copyright &copy; 2020
+        <a class="footertext" href="https://instagram.com/adam_traceurs" target="_blank"> Adam Roxann Irsyad</a>
+      </div>
+    </footer>
 	 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
